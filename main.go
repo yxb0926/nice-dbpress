03f9cacf0eecc18c720229c7f07f623a4d38c7bb
yxb <yxb0926@163.com>
2015-11-26 17:20:24 +0800

    xx

--- main.go
+++ main.go
@@ -1,41 +1,143 @@
 package main
 
 import "fmt"
-import "database/sql"
-import _ "github.com/go-sql-driver/mysql"
+import "os"
+import "github.com/larspensjo/config"
+import "flag"
+import "strconv"
 
-var c chan int
+var confile string
 
-func db_sel() {
-	//db, _ := sql.Open("mysql", "yxb:yxb@tcp(127.0.0.1:3306)/test?timeout=4s&charset=utf8")
-	//sql := "SELECT 1 AS id"
+type dsninfo struct {
+	hostname string
+	username string
+	password string
+	port     string
+	timeout  string
+	charset  string
+}
 
-	var i int = 0
-	for i < 100000 {
-		db, _ := sql.Open("mysql", "yxb:yxb@tcp(127.0.0.1:3306)/test?timeout=4s&charset=utf8")
-		sql := "SELECT 1 AS id"
-		rows := db.QueryRow(sql)
-		var id int
-		err := rows.Scan(&id)
-		if err != nil {
-			fmt.Println(err)
-		}
-		db.Close()
-		i++
-	}
-	c <- 1
+var dsn dsninfo
+var keepalived int = 1
+var currentnums int = 1
+var logfile string
+var sqlstr string
+
+const (
+	CURRENTNUMS = 8
+	KEEPALIVED  = 1
+	LOGFILE     = "/tmp/dbpress.log"
+)
+
+func init() {
+	// get the conf file
+	GetIniFile()
+	// Parse the conf file
+	ParseConfile("dbpress")
+
 }
 
 func main() {
-	c = make(chan int)
-	var i int = 0
-	for i < 100 {
-		go db_sel()
-		i++
+	fmt.Println("## main ")
+	fmt.Println(dsn)
+	fmt.Println(sqlstr)
+}
+
+func ParseConfile(key string) {
+	conf := make(map[string]string)
+	cfg, err := config.ReadDefault(confile)
+	if err != nil {
+		fmt.Errorf("Failed Read cfile")
 	}
-	var j int = 0
-	for j < 100 {
-		<-c
-		j++
+
+	if cfg.HasSection(key) {
+		section, err := cfg.SectionOptions(key)
+		if err == nil {
+			for _, v := range section {
+				options, err := cfg.String(key, v)
+				if err == nil {
+					conf[v] = options
+				}
+			}
+		}
+		if v, ok := conf["host"]; ok {
+			dsn.hostname = v
+		} else {
+			fmt.Errorf("Key:host Not Found In The Config file.")
+		}
+		if v, ok := conf["username"]; ok {
+			dsn.username = v
+		} else {
+			fmt.Errorf("Key:username Not Found In The Config file.")
+		}
+		if v, ok := conf["password"]; ok {
+			dsn.password = v
+		} else {
+			fmt.Errorf("Key:password Not Found In The Config file.")
+		}
+		if v, ok := conf["port"]; ok {
+			dsn.port = v
+		} else {
+			fmt.Errorf("Key:port Not Found In The Config file.")
+		}
+		if v, ok := conf["timeout"]; ok {
+			dsn.timeout = v
+		} else {
+			fmt.Errorf("Key:timeout Not Found In The Config file.")
+		}
+		if v, ok := conf["charset"]; ok {
+			dsn.charset = v
+		} else {
+			fmt.Errorf("Key:charset Not Found In The Config file.")
+		}
+		if v, ok := conf["currentnums"]; ok {
+			currentnums, _ = strconv.Atoi(v)
+		} else {
+			currentnums = CURRENTNUMS
+		}
+		if v, ok := conf["keepalived"]; ok {
+			keepalived, _ = strconv.Atoi(v)
+		} else {
+			currentnums = KEEPALIVED
+		}
+		if v, ok := conf["logfile"]; ok {
+			logfile = v
+		} else {
+			logfile = LOGFILE
+		}
+		if v, ok := conf["sqljson"]; ok {
+			sqlstr = v
+		} else {
+			fmt.Errorf("Key:sqljson Not Found In The Config file.")
+		}
+	} else {
+		fmt.Errorf("The conf file have no section %s", key)
 	}
+}
+
+func GetIniFile() string {
+	iniFile := flag.String("f", "iniFile", "The confile file name")
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		help()
+		os.Exit(0)
+	}
+
+	if _, err := os.Stat(*iniFile); err == nil {
+		confile = *iniFile
+	} else {
+		fmt.Println("The confile", *iniFile, "not exist.")
+		os.Exit(7)
+	}
+
+	return confile
+}
+
+func help() {
+	var helpx string = `dbpress usage:
+	-f="iniFile": The confile file name
+	`
+
+	fmt.Println(helpx)
 }
